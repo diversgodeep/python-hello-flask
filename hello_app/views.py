5dabfd834462e2c7052d58006d87bc52b5f0c3cd
DeVesco Pierre <68120998+DPtheblockchainingdevop@users.noreply.github.com>
2020-09-28 11:52:35 -0600

    Refactor the project to support further development

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,14 +1,22 @@
-from flask import Flask
 from datetime import datetime
-from flask import render_template
-import re
 
-app = Flask(__name__)
+from flask import Flask, render_template
+
+from . import app
 
 @app.route("/")
 def home():
   return render_template("home.html")
 
+@app.route("/about/")
+def about():
+  return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+  return render_template("contact.html")
+
+@app.route("/hello/")
 @app.route("/hello/<name>")
 def hello_there(name = None):
   return render_template(
@@ -20,11 +28,3 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
   return app.send_static_file("data.json")
-
-@app.route("/about/")
-def about():
-  return render_template("about.html")
-
-@app.route("/contact/")
-def contact():
-  return render_template("contact.html")
